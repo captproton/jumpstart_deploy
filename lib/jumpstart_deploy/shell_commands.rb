4bed54f82e484589ffaae1d4316ef3177522735b
captproton <carl@wdwhub.net>
2024-12-01 13:02:31 -0800

    Simplify command execution following project security guidelines

--- lib/jumpstart_deploy/shell_commands.rb
+++ lib/jumpstart_deploy/shell_commands.rb
@@ -5,153 +5,71 @@ module JumpstartDeploy
     class CommandError < StandardError; end
     class InvalidCommandError < CommandError; end
 
-    # Define allowed binaries and their full paths
-    ALLOWED_BINARIES = {
-      "git" => "/usr/bin/git",           # Standard git location
-      "bundle" => "/usr/local/bin/bundle", # Typical RVM/rbenv location
-      "bin/rails" => "bin/rails"          # Project-relative Rails binary
-    }.freeze
-
-    # Define command configurations with their exact argument patterns
-    COMMAND_CONFIG = {
+    # Define allowed commands with their allowed arguments
+    COMMAND_WHITELIST = {
       "git" => {
-        "clone" => {
-          args: [:url, :path],
-          validator: ->(args) { args.length == 2 }
-        },
-        "remote" => {
-          args: [:action, :name, :url],
-          validator: ->(args) { ["add", "remove"].include?(args[0]) && args.length.between?(2, 3) }
-        },
-        "add" => {
-          args: [:path],
-          validator: ->(args) { args.length == 1 }
-        },
-        "commit" => {
-          args: ["-m", :message],
-          validator: ->(args) { args.length == 2 && args[0] == "-m" }
-        },
-        "push" => {
-          args: [:remote, :branch],
-          validator: ->(args) { args.length == 2 }
-        }
+        "clone" => 2,      # url, path
+        "remote" => 2..3,  # add/remove, name, [url]
+        "add" => 1,        # path
+        "commit" => 2,     # -m, message
+        "push" => 2        # remote, branch
       },
       "bundle" => {
-        "install" => {
-          args: [],
-          validator: ->(args) { args.empty? }
-        },
-        "exec" => {
-          args: [:command],
-          validator: ->(args) { !args.empty? }
-        }
+        "install" => 0,
+        "exec" => 1
       },
       "bin/rails" => {
-        "db:create" => {
-          args: [],
-          validator: ->(args) { args.empty? }
-        },
-        "db:migrate" => {
-          args: [],
-          validator: ->(args) { args.empty? }
-        },
-        "assets:precompile" => {
-          args: [],
-          validator: ->(args) { args.empty? }
-        }
+        "db:create" => 0,
+        "db:migrate" => 0,
+        "assets:precompile" => 0
       }
     }.freeze
 
     def self.execute(command, subcommand, *args, dir: nil)
-      cmd_config = validate_and_get_config!(command, subcommand, args)
-      cmd_array = build_command_array(command, subcommand, args, cmd_config)
-
-      run_command(cmd_array, dir)
+      validate_command!(command, subcommand, args)
+      Dir.chdir(dir || Dir.pwd) do
+        out, err, status = Open3.capture3(command, subcommand, *args)
+        unless status.success?
+          raise CommandError, "Command failed: #{err}"
+        end
+        out
+      end
     end
 
-    def self.validate_and_get_config!(command, subcommand, args)
-      unless COMMAND_CONFIG.key?(command)
+    def self.validate_command!(command, subcommand, args)
+      unless COMMAND_WHITELIST.key?(command)
         raise InvalidCommandError, "Command not allowed: #{command}"
       end
 
-      cmd_configs = COMMAND_CONFIG[command]
-      unless cmd_configs.key?(subcommand)
-        raise InvalidCommandError, "Subcommand not allowed: #{subcommand} for #{command}"
+      allowed_args = COMMAND_WHITELIST[command][subcommand]
+      case allowed_args
+      when Integer
+        unless args.length == allowed_args
+          raise InvalidCommandError, "Invalid number of arguments"
+        end
+      when Range
+        unless allowed_args.include?(args.length)
+          raise InvalidCommandError, "Invalid number of arguments"
+        end
       end
 
-      config = cmd_configs[subcommand]
-      unless config[:validator].call(args)
-        raise InvalidCommandError, "Invalid arguments for #{command} #{subcommand}"
-      end
-
-      config
-    end
-
-    def self.build_command_array(command, subcommand, args, config)
-      validated_command = get_binary_path(command)
-      validated_args = process_arguments(args)
-      [validated_command, subcommand, *validated_args].compact
-    end
-
-    def self.get_binary_path(command)
-      unless ALLOWED_BINARIES.key?(command)
-        raise InvalidCommandError, "Binary not allowed: #{command}"
-      end
-      ALLOWED_BINARIES[command]
-    end
-
-    def self.process_arguments(args)
-      args.map(&:to_s).map do |arg|
-        if arg.match?(/[;&|]/)
+      args.each do |arg|
+        if arg.to_s.match?(/[;&|]/)
           raise InvalidCommandError, "Invalid characters in argument"
         end
-        arg
       end
     end
 
-    def self.run_command(cmd_array, dir = nil)
-      working_dir = dir || Dir.pwd
-      read_pipe, write_pipe = IO.pipe
-
-      Dir.chdir(working_dir) do
-        pid = fork do
-          read_pipe.close
-          $stdout.reopen(write_pipe)
-          $stderr.reopen(write_pipe)
-          write_pipe.close
-
-          # Clean environment and set minimal PATH
-          ENV.clear
-          ENV["PATH"] = "/usr/local/bin:/usr/bin:/bin"
-          
-          # Execute with explicit binary path
-          exec(*cmd_array, unsetenv_others: true)
-        end
-
-        write_pipe.close
-        output = read_pipe.read
-        read_pipe.close
-
-        _, status = Process.waitpid2(pid)
-        
-        unless status.success?
-          raise CommandError, "Command failed: #{output}"
-        end
-
-        output
-      end
+    def self.git(*args, dir: nil)
+      execute("git", *args, dir: dir)
     end
 
-    def self.git(subcommand, *args, dir: nil)
-      execute("git", subcommand, *args, dir: dir)
+    def self.rails(*args, dir: nil)
+      execute("bin/rails", *args, dir: dir)
     end
 
-    def self.rails(subcommand, *args, dir: nil)
-      execute("bin/rails", subcommand, *args, dir: dir)
-    end
-
-    def self.bundle(subcommand, *args, dir: nil)
-      execute("bundle", subcommand, *args, dir: dir)
+    def self.bundle(*args, dir: nil)
+      execute("bundle", *args, dir: dir)
     end
   end
 end
