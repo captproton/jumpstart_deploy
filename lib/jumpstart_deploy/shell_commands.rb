3b90ecceb6381c17c49f204815e3089ee5e87cd8
captproton <carl@wdwhub.net>
2024-12-01 12:47:54 -0800

    Implement strict command pattern validation and execution

--- lib/jumpstart_deploy/shell_commands.rb
+++ lib/jumpstart_deploy/shell_commands.rb
@@ -1,77 +1,107 @@
 # frozen_string_literal: true
 
 require "open3"
-require "shellwords"
 
 module JumpstartDeploy
   module ShellCommands
     class CommandError < StandardError; end
     class InvalidCommandError < CommandError; end
 
-    # Define allowed commands with their allowed arguments
-    COMMAND_WHITELIST = {
+    # Define command configurations with their exact argument patterns
+    COMMAND_CONFIG = {
       "git" => {
-        "clone" => 2,      # url, path
-        "remote" => 2..3,  # add/remove, name, [url]
-        "add" => 1..2,     # [options], path
-        "commit" => 2,     # -m, message
-        "push" => 2..3     # -u, origin, main
+        "clone" => {
+          args: [:url, :path],
+          validator: ->(args) { args.length == 2 }
+        },
+        "remote" => {
+          args: [:action, :name, :url],
+          validator: ->(args) { ["add", "remove"].include?(args[0]) && args.length.between?(2, 3) }
+        },
+        "add" => {
+          args: [:path],
+          validator: ->(args) { args.length == 1 }
+        },
+        "commit" => {
+          args: ["-m", :message],
+          validator: ->(args) { args.length == 2 && args[0] == "-m" }
+        },
+        "push" => {
+          args: [:remote, :branch],
+          validator: ->(args) { args.length == 2 }
+        }
       },
       "bundle" => {
-        "install" => 0,    # no args
-        "exec" => 1..Float::INFINITY # command + args
+        "install" => {
+          args: [],
+          validator: ->(args) { args.empty? }
+        },
+        "exec" => {
+          args: [:command],
+          validator: ->(args) { !args.empty? }
+        }
       },
       "bin/rails" => {
-        "db:create" => 0,
-        "db:migrate" => 0,
-        "assets:precompile" => 0
+        "db:create" => {
+          args: [],
+          validator: ->(args) { args.empty? }
+        },
+        "db:migrate" => {
+          args: [],
+          validator: ->(args) { args.empty? }
+        },
+        "assets:precompile" => {
+          args: [],
+          validator: ->(args) { args.empty? }
+        }
       }
     }.freeze
 
     def self.execute(command, subcommand, *args, dir: nil)
-      validate_command!(command, subcommand, args)
+      cmd_config = validate_and_get_config!(command, subcommand, args)
+      cmd_array = build_command_array(command, subcommand, args, cmd_config)
 
-      cmd_array = [ command, subcommand, *args ].compact.map(&:to_s)
+      run_command(cmd_array, dir)
+    end
 
+    def self.validate_and_get_config!(command, subcommand, args)
+      unless COMMAND_CONFIG.key?(command)
+        raise InvalidCommandError, "Command not allowed: #{command}"
+      end
+
+      cmd_configs = COMMAND_CONFIG[command]
+      unless cmd_configs.key?(subcommand)
+        raise InvalidCommandError, "Subcommand not allowed: #{subcommand} for #{command}"
+      end
+
+      config = cmd_configs[subcommand]
+      unless config[:validator].call(args)
+        raise InvalidCommandError, "Invalid arguments for #{command} #{subcommand}"
+      end
+
+      config
+    end
+
+    def self.build_command_array(command, subcommand, args, config)
+      [command, subcommand, *process_arguments(args)].compact
+    end
+
+    def self.process_arguments(args)
+      args.map(&:to_s).map do |arg|
+        if arg.match?(/[;&|]/)
+          raise InvalidCommandError, "Invalid characters in argument"
+        end
+        arg
+      end
+    end
+
+    def self.run_command(cmd_array, dir = nil)
       Dir.chdir(dir || Dir.pwd) do
         out, err, status = Open3.capture3(*cmd_array)
         unless status.success?
           raise CommandError, "Command failed: #{err}"
         end
         out
-      end
-    end
-
-    def self.validate_command!(command, subcommand, args)
-      unless COMMAND_WHITELIST.key?(command)
-        raise InvalidCommandError, "Command not allowed: #{command}"
-      end
-
-      allowed_subcommands = COMMAND_WHITELIST[command]
-      unless allowed_subcommands.key?(subcommand)
-        raise InvalidCommandError, "Subcommand not allowed: #{subcommand} for #{command}"
-      end
-
-      allowed_args = allowed_subcommands[subcommand]
-      case allowed_args
-      when Integer
-        unless args.length == allowed_args
-          raise InvalidCommandError, "Invalid number of arguments for #{command} #{subcommand}"
-        end
-      when Range
-        unless allowed_args.include?(args.length)
-          raise InvalidCommandError, "Invalid number of arguments for #{command} #{subcommand}"
-        end
-      end
-
-      validate_arguments!(args)
-    end
-
-    def self.validate_arguments!(args)
-      args.each do |arg|
-        if arg.to_s.match?(/[;&|]/)
-          raise InvalidCommandError, "Invalid characters in argument"
-        end
       end
     end
 
