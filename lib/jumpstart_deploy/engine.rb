733c7607fe7a7659b1bde7c3ddf1f0c88cfc8d5b
captproton <carl@wdwhub.net>
2024-12-14 13:24:22 -0800

    fix: resolve merge conflict in engine.rb

--- lib/jumpstart_deploy/engine.rb
+++ lib/jumpstart_deploy/engine.rb
@@ -1,7 +1,15 @@
+# frozen_string_literal: true
+
 module JumpstartDeploy
   if defined?(Rails)
     class Engine < ::Rails::Engine
       isolate_namespace JumpstartDeploy
+
+      config.generators do |g|
+        g.test_framework :rspec
+        g.fixture_replacement :factory_bot
+        g.factory_bot dir: "spec/factories"
+      end
 
       # Include view helpers
       initializer "jumpstart_deploy.helpers" do
